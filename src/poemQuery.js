3e84c8aed98bcf5608fc99b54861a8ec23f153ea
Nikhil Nawgiri <the@realest.ninja>
2023-05-13 19:10:59 +0200

    Refactor poemPromptInstructions

--- src/poemQuery.js
+++ src/poemQuery.js
@@ -10,9 +10,11 @@ export const getPoemPrompt = ({
   poemLineCount,
 }) => {
   const poemPromptInstructions = [
-    `Write a ${poemLineCount > 12 ? 12 : poemLineCount}line poem about a movie or series and make sure that it rhymes.`,
+    `Write a ${poemLineCount > 12 ? 12 : poemLineCount}line poem about a movie or series.`,
     `The subject for your poem shall be ${itemTitle} which has the imdb id ${imdbId}.`,
+    "Make sure that it rhymes!",
     "Do not ever mention the imdb id!",
+    "Do not ignore the amount of lines specified!",
   ];
 
   if (mood in moodInstructions) {
